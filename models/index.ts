7115261df19803b0da6cb31098485fd5e5f57662
diego <diego.herrera@tekchoice.com>
2022-12-20 09:10:36 -0600

    WIP man-shipping error

--- models/index.ts
+++ models/index.ts
@@ -3,4 +3,4 @@ export {default as Rol} from './rol';
 export {default as Category} from './category';
 export {default as Product} from './products';
 export {default as Shipping} from './shipping';
-// export {default as ManualShipping} from './manualShipping';
+export {default as ManualShipping} from './manualShipping';
