d57e4c36dcbe7d0f5c85629ae74445146906bff8
diego <diego.herrera@tekchoice.com>
2022-12-20 08:27:28 -0600

    WIP fix bonding

--- models/bondingCompany.ts
+++ models/bondingCompany.ts
@@ -1,17 +1,17 @@
-import {model, Schema} from "mongoose";
-
-
-const BondingCompanySchema = new Schema({
-  user: Schema.Types.ObjectId,
-  company: Schema.Types.ObjectId
-})
-
-
-
-BondingCompanySchema.methods.toJSON = function(){
-  const {__v ,_id, ...rest } = this.toObject();
-
-  return {id:_id,...rest};
-}
-
-export default model('BondingCompany', BondingCompanySchema);
+// import {model, Schema} from "mongoose";
+//
+//
+// const BondingCompanySchema = new Schema({
+//   user: Schema.Types.ObjectId,
+//   company: Schema.Types.ObjectId
+// })
+//
+//
+//
+// BondingCompanySchema.methods.toJSON = function(){
+//   const {__v ,_id, ...rest } = this.toObject();
+//
+//   return {id:_id,...rest};
+// }
+//
+// export default model('BondingCompany', BondingCompanySchema);
